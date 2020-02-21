1555d9d0436f8356725671175a42bce03bc0bc5d
Daniel Kmak <neverkuzi@gmail.com>
2020-02-21 02:46:40 +0100

    Rename deploy parameter

--- src/common.ts
+++ src/common.ts
@@ -2,14 +2,14 @@ import Web3 from 'web3';
 import { HeadTail } from './types/HeadTail';
 import * as HeadTailJSON from '../build/contracts/HeadTail.json';
 
-export async function deployHeadTailContract(web3: Web3, defaultSender: string): Promise<HeadTail> {
+export async function deployHeadTailContract(web3: Web3, account: string): Promise<HeadTail> {
     const HeadTailContract: HeadTail = new web3.eth.Contract(HeadTailJSON.abi as any) as any;
 
     return HeadTailContract.deploy({
         data: HeadTailJSON.bytecode,
         arguments: []
     }).send({
-        from: defaultSender,
+        from: account,
         gas: 6000000
     });
 }
