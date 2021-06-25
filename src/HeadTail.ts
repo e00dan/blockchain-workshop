04b115040cb3e9486db27675022320ecfbaa45d8
Daniel Kmak <neverkuzi@gmail.com>
2021-06-25 17:28:34 +0200

    update code

--- src/HeadTail.ts
+++ src/HeadTail.ts
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-use-before-define */
 import Web3 from 'web3';
+import PolyjuiceHttpProviderForNode from '@retric/test-provider/lib/cli';
 import { CONFIG } from './config';
 import {
     deployHeadTailContract,
@@ -9,9 +10,6 @@ import {
     domainSeparator,
     useExistingHeadTailContract
 } from './common';
-// import {  } from '@retric/test-provider';
-
-const PolyjuiceHttpProviderForNode = require('@retric/test-provider/lib/cli');
 
 const godwoken_rpc_url = 'http://localhost:8024';
 const provider_config = {
@@ -19,19 +17,17 @@ const provider_config = {
         rollup_type_hash: '0x0a30665c3047d65cb3651eda93182a0d2f2087317aaba3ab35f3a970089ea9b4',
         eth_account_lock: {
             code_hash: '0x075bf74f81f492a620dc29a6193b8d66d8d351e486992141efbfce7fda5862b5',
-            hash_type: 'type'
+            hash_type: 'type' as any
         }
     }
 };
-// const oneEther = BigInt(1 * 10 ** 18).toString();
 
 const ONLY_DEPLOY_AND_STOP = false;
 
 const BET_VALUE = BigInt(10 * 10 ** 8).toString(); // 10 CKB
 const USER_ONE_PRIVATE_KEY = '0xd9066ff9f753a1898709b568119055660a77d9aae4d7a4ad677b8fb3d2a571e5';
 const USER_TWO_PRIVATE_KEY = '0xd9bc30dc17023fbb68fe3002e0ff9107b241544fd6d60863081c55e383f1b5a3';
-// N: 0x739a766a3680c77adfa38908e8899a9abe37d14f29e77af2339eb9b158844a1d7132f9e5b3a7196f9a321288e241865d0ff3cf5facc09a317af5160e0600b48301
-// B: 0x7c42ff81eb095e98dfba8ff1195518e188a30cd97a7fa01e9d83b580f7710ae03f25b1702742a0d0970b6573f70c412535db01de1d2a0b724f1ac29d2af71fbd00
+
 const userOneEthAddress = '0xD173313A51f8fc37BcF67569b463abd89d81844f';
 const userTwoEthAddress = '0xd46aC0Bc23dB5e8AfDAAB9Ad35E9A3bA05E092E8';
 
