53c69005a17112fc732c4ad92a435119da79d520
Daniel Kmak <neverkuzi@gmail.com>
2021-11-03 16:48:12 +0100

    Remove node-sass

--- webpack.config.js
+++ webpack.config.js
@@ -61,9 +61,6 @@ module.exports = {
                     },
                     {
                         loader: 'css-loader' // translates CSS into CommonJS
-                    },
-                    {
-                        loader: 'sass-loader' // compiles Sass to CSS
                     }
                 ]
             },
